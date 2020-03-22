d7a99800f7b01ce5bccb900a70a96678beb4514a
Akshay <akshay.solanki@clevertap.com>
2020-03-22 20:18:41 +0530

    image changed

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,7 +5,11 @@
       <v-col cols="12">
         <v-row align="center" justify="center">
           <v-col align="center" justify="center">
-            <v-img src="/my_image.jpg" aspect-ratio="1" class="image"></v-img>
+            <v-img
+              src="@/assets/my_image.jpg"
+              aspect-ratio="1"
+              class="image"
+            ></v-img>
           </v-col>
         </v-row>
         <v-row>
